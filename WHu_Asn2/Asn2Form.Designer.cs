df7f2fe47a299f2512e1dd4af4daec2953c24d69
wozenwho <wilson.huu@gmail.com>
2018-01-22 00:10:04 -0800

    First commit

---
+++ WHu_Asn2/Asn2Form.Designer.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace WHu_Asn2
+{
+    class Asn2Form : Form
+    {
+        static int squareWidth = 10;
+        static int borderWidth = 15;
+        static Boolean wat = false;
+
+        public Asn2Form()
+        {
+            Text = "C4560: Assignment 2, Exercise 1 (Wilson Hu 4-O / 2018)";
+            BackColor = Color.Black;
+            ResizeRedraw = true;
+
+            SetStyle(ControlStyles.UserPaint, true);
+            SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+            SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+
+        }
+
+        protected override void OnPaint(PaintEventArgs pea)
+        {
+            Graphics grfx = pea.Graphics;
+            SolidBrush brush = new SolidBrush(Color.Green);
+            SolidBrush backgroundBrush = new SolidBrush(Color.White);
+            grfx.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            grfx.PixelOffsetMode = System.Drawing.Drawing2D.PixelOffsetMode.HighQuality;
+            if (wat)
+            {
+                grfx.FillRectangle(backgroundBrush, ClientSize.Width / 2, 0, ClientSize.Width / 2, ClientSize.Height);
+            }
+            else
+            {
+                grfx.FillRectangle(backgroundBrush, 0, 0, ClientSize.Width / 2, ClientSize.Height);
+            }
+            
+            int midX = ClientSize.Width / 2;
+            int cx = midX;
+            int cy = borderWidth;
+
+
+            while (cy < (ClientSize.Height - borderWidth))
+            {
+                while (cx < (ClientSize.Width - borderWidth))
+                {
+                    grfx.FillRectangle(brush, cx, cy, squareWidth, squareWidth);
+                    grfx.FillRectangle(brush, midX - squareWidth - (cx - midX), cy, squareWidth, squareWidth);
+                    cx += (int)(1.5 * squareWidth);
+                }
+                cy += (int) (1.5 * squareWidth);
+                cx = midX;
+            }
+        }
+
+        protected override void OnClick(EventArgs e)
+        {
+            base.OnClick(e);
+            wat = !wat;
+            this.Invalidate();
+        }
+
+
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Text = "Form1";
+        }
+
+        #endregion
+    }
+}
+
---
+++ WHu_Asn2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+namespace WHu_Asn2
+{
+    class Program : Form
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Asn2Form());
+
+        }
+    }
+}
